23e221c6af1c85e9a385ea7f6221dc9b4a4988c2
adilier <adlerdet@outlook.com>
2019-11-19 14:53:17 -0300

    Adicionando o header e o slider

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Container, Menu, Search } from "./styles";
+
+import logo from "../../assets/logos/logo.svg";
+
+const Header = function() {
+  return (
+    <Container>
+      <div id="menuWrapper">
+        <img className="brand-logo" src={logo} alt="logo" />
+        <Menu>
+          <button className="active-link">Início</button>
+          <button>Séries</button>
+          <button>Filmes</button>
+          <button>Infantil</button>
+        </Menu>
+      </div>
+      <div id="actions">
+        <Search>
+          <input type="text" placeholder="Busca" />
+        </Search>
+        <i className="fa fa-user user-icon" />
+        <i className="fa fa-caret-down arrow-icon" />
+      </div>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Slider/styles.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+import PrevIcon from "../../assets/logos/prev.svg";
+import NextIcon from "../../assets/logos/next.svg";
+import IndicatorIcon from "../../assets/logos/circle.svg";
+
+export const Container = styled.div`
+  width: 100%;
+  .containerImg{
+    display: flex;
+    justify-content: center;
+    img {
+      width: 85%;
+    }
+  }
+  #controls {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+    z-index: 5;
+    width: 100%;
+    margin: 100px 0;
+    .next-button {
+      width: 30px;
+      height: 30px;
+      background: url(${NextIcon}) center no-repeat;
+      background-size: 25px 25px;
+      padding: 60px 0px;
+    }
+    .prev-button {
+      width: 30px;
+      height: 30px;
+      background: url(${PrevIcon}) center no-repeat;
+      background-size: 25px 25px;
+      padding: 60px 0px;
+    }
+  }
+  #indicators {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    z-index: 6;
+    width: 100%;
+    margin: 100px 0;
+    .indicator {
+      width: 10px;
+      height: 10px;
+      background: url(${IndicatorIcon}) center no-repeat;
+      background-size: 10px 10px;
+      padding: 10px;
+      opacity: 0.5;
+      &:hover {
+        opacity: 1;
+      }
+    }
+    .active {
+      opacity: 1;
+    }
+  }
+`;
